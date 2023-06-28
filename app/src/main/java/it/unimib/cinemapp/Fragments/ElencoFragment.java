d1f624c444f510eaa26f364acaa19ee57283236f
TeoDiga <107485108+TeoDiga@users.noreply.github.com>
2023-06-28 23:34:18 +0200

    espanso l'elenco

--- app/src/main/java/it/unimib/cinemapp/Fragments/ElencoFragment.java
+++ app/src/main/java/it/unimib/cinemapp/Fragments/ElencoFragment.java
@@ -5,12 +5,19 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import it.unimib.cinemapp.Model.Film;
 import it.unimib.cinemapp.R;
+import it.unimib.cinemapp.Util.FilmMultipliRecyclerViewAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +26,8 @@ import it.unimib.cinemapp.R;
  */
 public class ElencoFragment extends Fragment {
 
+    private List<Film> listFilm;
+    private FilmMultipliRecyclerViewAdapter adapter;
 
     public ElencoFragment() {
         // Required empty public constructor
@@ -31,6 +40,7 @@ public class ElencoFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        listFilm= new ArrayList<>();
     }
 
     @Override
@@ -44,5 +54,17 @@ public class ElencoFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        RecyclerView recyclerView=view.findViewById(R.id.recyclerViewRisultati);
+        RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(requireContext(),
+                LinearLayoutManager.VERTICAL, false);
+        FilmMultipliRecyclerViewAdapter filmMultipliRecyclerViewAdapter= new FilmMultipliRecyclerViewAdapter(listFilm,
+                new FilmMultipliRecyclerViewAdapter.OnItemClickListener() {
+                    @Override
+                    public void onFilmClick(Film film) {
+                        //lanciare il nuovo fragment
+                    }
+                });
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(filmMultipliRecyclerViewAdapter);
     }
 }
